ee8e4417e44006248a32d4e2e85f267c63f9c151
Ryan Woo <rjwoo@sfu.ca>
2017-04-04 12:25:23 -0400

    first commit

---
+++ Q2.js
@@ -0,0 +1,9 @@
+// Write a function that converts user entered date formatted as M/D/YYYY to a format required by an API (YYYYMMDD). The parameter "userDate" and the return value are strings.
+
+//For example, it should convert user entered date "12/31/2014" to "20141231" suitable for the API.
+
+function formatDate(userDate) {
+  // format from M/D/YYYY to YYYYMMDD
+}
+
+console.log(formatDate("12/31/2014"));
---
+++ Q3.php
@@ -0,0 +1,16 @@
+<!-- A palindrome is a word that reads the same backward or forward.
+
+Write a function that checks if a given word is a palindrome. Character case should be ignored.
+
+For example, isPalindrome("Deleveled") should return true as character case should be ignored, resulting in "deleveled", which is a palindrome since it reads the same backward and forward. -->
+
+<?php
+class Palindrome
+{
+    public static function isPalindrome($word)
+    {
+        return NULL;
+    }
+}
+
+echo Palindrome::isPalindrome('Deleveled');
